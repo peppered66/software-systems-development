ee895b2f07ce0759e76cc635e2321bc008597aff
earisk <W.Smith6872@student.leedsbeckett.ac.uk>
2024-09-25 10:47:12 +0100

    First commit.
    Created folders for first two weeks of work and started first program.

---
+++ SSD Week 1/src/Rectangle.java
@@ -0,0 +1,26 @@
+
+public class Rectangle extends Shape {
+	private int height;
+	private int width;
+	
+	Rectangle(int sides) {
+		super(sides);
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public int getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ SSD Week 1/src/Shape.java
@@ -0,0 +1,19 @@
+
+abstract class Shape {
+	
+	private int sides;
+	
+	public int getSides() {
+		return sides;
+	}
+	
+	public void setSides(int sides) {
+		this.sides = sides;
+	}
+	
+	abstract public int getArea();
+	
+	Shape(int sides) {
+		this.sides = sides;
+	}
+}

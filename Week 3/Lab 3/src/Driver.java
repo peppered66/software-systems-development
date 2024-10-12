d4678217630cc25e51d30e6a66650120a66e8ed6
earisk <W.Smith6872@student.leedsbeckett.ac.uk>
2024-10-12 15:16:01 +0100

    Completed week 3 program.
    Focus was mainly on documentating the program and correcting the errors.
    Leading to the program being easier to understand which is important in
    a professional setting.

--- Week 3/Lab 3/src/Driver.java
+++ Week 3/Lab 3/src/Driver.java
@@ -1,24 +1,40 @@
 import java.util.Random;
 
-
+/**
+ * a driver/main class which incorporates the {@link Stats} class.
+ * 
+ * @author peppered66
+ * 
+ */
 public class Driver {
-	public static void main(String[] args) {final int VALUES = 10;
+	/**
+	 * Main method where program starts to execute
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Constant value which defines number of values to be added
+		final int VALUES = 10;
 
+		// creates instance of Stats class assigning the constant value as our parameter
 		Stats stats = new Stats(VALUES);
 
-Random random = new Random();
-		
+		// creates instance of random class to generate values
+		Random random = new Random();
+
+		// randomly generated values are added to the list
 		for (int i = 0; i < VALUES; i++) {
-			
-stats.addValue(random.nextInt(100));
+			stats.addValue(random.nextInt(100));
 		}
 
-System.out.println("Numbers stored : ");
+		// Outputting information using the methods from stats class
+		System.out.println("Numbers stored : ");
 		System.out.println(stats);
-		
-System.out.println("Average = " + stats.getAverage());
+
+		System.out.println("Average = " + stats.getAverage());
 		System.out.println("Count = " + stats.getCount());
-	System.out.println("Total = " + stats.getTotal());
-		System.out.println("Minimum value = " + stats.getB());
-System.out.println("Maximum value = " + stats.getA());
-	}}
+		System.out.println("Total = " + stats.getTotal());
+		System.out.println("Minimum value = " + stats.getMinimum());
+		System.out.println("Maximum value = " + stats.getMax());
+	}
+}
--- Week 3/Lab 3/src/Stats.java
+++ Week 3/Lab 3/src/Stats.java
@@ -1,75 +1,122 @@
 import java.util.Arrays;
 
-
+/**
+ * A class which calculates various statistical data from a list of numbers.
+ * 
+ * @author peppered66
+ */
 public class Stats {
 
-int[] numbers;
-	int count;
+	/**
+	 * Array to store list of numbers
+	 */
+	private int[] numbers;
 
-	
+	/***
+	 * numbers of values currently stored in list {@link #numbers}.
+	 */
+	private int count;
+
+	/**
+	 * this method will add values to the list
+	 * 
+	 * @param value the value to be added to list
+	 */
 	public void addValue(int value) {
-numbers[count] = value;
-count++;}
-
-	public int getCount() {
-return numbers.length;
+		// adds the given value to the array
+		numbers[count] = value;
+		// increments count
+		count++;
 	}
 
-	public int getA() {
+	/**
+	 * Gets the number of currently stored values
+	 * 
+	 * @return the number of values stored
+	 */
+	public int getCount() {
+		return count;
+	}
 
-		int max = numbers[0];	
+	/**
+	 * this method will calculate and return the highest value
+	 * 
+	 * @return the largest value in list
+	 */
+	public int getMax() {
+		int max = numbers[0]; // start with first value
 
+		// goes through list and finds highest value
 		for (int i = 1; i < numbers.length; i++) {
-if (numbers[i] > max)				max = numbers[i];		}
+			if (numbers[i] > max)
+				max = numbers[i];
+		}
 
 		return max;
 	}
 
-	public int getB() {int min = numbers[0]; 
+	/**
+	 * this method will calculate and return the lowest value
+	 * 
+	 * @return the lowest value in list
+	 */
+	public int getMinimum() {
+		int min = numbers[0]; // start with first value
 
-	for (int i = 1; i < numbers.length; i++) {
+		// goes through list and finds lowest value
+		for (int i = 1; i < numbers.length; i++) {
 
-if (numbers[i] < min)
-			min = numbers[i];
+			if (numbers[i] < min)
+				min = numbers[i];
 		}
 
 		return min;
 	}
-	
+
+	/**
+	 * this method will add all values to get total value and return this total s
+	 * 
+	 * @return the sum of all values in list
+	 */
 	public int getTotal() {
-		
-		int total = 0;
-		
-		// total all values within the array
+
+		int total = 0; // start with 0
+
+		// calculates total of all values within the list
 		for (int i = 0; i < numbers.length; i++) {
 			total += numbers[i];
-		}		return total;
+		}
+		return total;
 	}
-	
+
+	/**
+	 * this method calculates the average from the values and returns the value
+	 * 
+	 * @return the average value of the list
+	 */
 	public double getAverage() {
-		
 		int x = 0;
-				for (int i = 0; i < numbers.length; i++) {
+		// adds all values in array together
+		for (int i = 0; i < numbers.length; i++) {
 			x += numbers[i];
 		}
-		
-		double y = x/(double)numbers.length;
-		
-return y;
+		// divides total value by number of values present in list to get the average
+		double y = x / (double) numbers.length;
+
+		return y;
 	}
 
-	@Override
-	public String toString() {return Arrays.toString(numbers);
+	public String toString() {
+		return Arrays.toString(numbers);
 	}
-	
 
+	/**
+	 * constructor
+	 * 
+	 * @param capacity the capacity of max values in list allowed
+	 */
 	public Stats(int capacity) {
-numbers = new int[capacity];
+		numbers = new int[capacity];
 	}
-	
-	
-	
-	
-	
-	
+
 }

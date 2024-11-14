7866184ffee439fa97928125b1f0aeb98962c4ac
earisk <W.Smith6872@student.leedsbeckett.ac.uk>
2024-11-14 17:51:48 +0000

    Completed week 7 work, generated javadoc and cleaned up file system.

--- Week 7/Lab 7/src/Main.java
+++ Week 7/Lab 7/src/Main.java
@@ -16,6 +16,20 @@ public class Main {
 		manager.printNames();
 		// should now have longer names removed
 
+		// Test the Number Sorter
+
+		NumberSorter sorter = new NumberSorter();
+
+		// test ascending
+		sorter.sort(new int[] { 1, 9, 2, 3, 10, 8, 12, 1, 99, 2, 43, 68, 109, 0 }, true);
+		sorter.sort(new int[] { 1, 2, 3, 4 }, true);
+		sorter.sort(new int[] { 4, 3, 2, 1 }, true);
+
+		// test descending
+		sorter.sort(new int[] { 1, 9, 2, 3, 10, 8, 12, 1, 99, 2, 43, 68, 109, 0 }, false);
+		sorter.sort(new int[] { 1, 2, 3, 4 }, false);
+		sorter.sort(new int[] { 4, 3, 2, 1 }, false);
+
 	}
 
 }
--- Week 7/Lab 7/src/NumberSorter.java
+++ Week 7/Lab 7/src/NumberSorter.java
@@ -1,4 +1,51 @@
+import java.util.Stack;
 
+/**
+ * A simple number sorter based on the use of two stacks
+ * 
+ * @author peppered66
+ */
 public class NumberSorter {
+
+	/**
+	 * Sorts the given array of integers into order, then outputs to the console
+	 * 
+	 * @param numbers the numbers to be sorted and output
+	 * @param ascending a flag that indicates whether to sort into ascending or descending order
+	 */
+	void sort(int[] numbers, boolean ascending) {
+
+		// The source stack, to which the numbers are popped
+		Stack<Integer> srcStack = new Stack<Integer>();
+		
+		// The dest stack, from which the numbers are pushed
+		Stack<Integer> destStack = new Stack<Integer>();
+
+		// Add the initial array of numbers to the source stack.
+		for (int number : numbers)
+			srcStack.push(number);
+
+		// Iterate while the source stack still contains number
+		while (!srcStack.isEmpty()) {
+
+			int next = srcStack.pop();
+
+			// Pop values from the dest stack which are greater (if ascending) or less than (if descending) than the next number, 
+			// and push these back onto the source stack
+			while (!destStack.isEmpty() && ((ascending && destStack.peek() > next) || (!ascending && destStack.peek() < next)))
+				srcStack.push(destStack.pop());
+
+			// push the next number to the dest stack (once all greater values have been popped)
+			destStack.push(next);
+		}
+
+		// Print the contents of the dest stack (which now will be ordered)
+		System.out.print("Sorted: ");
+		
+		for (int number : destStack)
+			System.out.print(number + " ");
+
+		System.out.println();
+	}
 
 }
